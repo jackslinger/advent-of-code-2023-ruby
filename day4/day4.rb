14607327f2735f7cc40a22f26c7c52ab9cb05912
Jack Slinger <jack.slinger@epigenesys.org.uk>
2023-12-11 13:31:06 +0000

    An attempt at day 4 part 2.

--- day4/day4.rb
+++ day4/day4.rb
@@ -16,9 +16,7 @@ class Card
   end
 
   def score
-    count = chosen.select do |chosen_number|
-      winning.include?(chosen_number)
-    end.size
+    count = number_of_matches
     if count > 0
       2 ** (count - 1)
     else
@@ -26,11 +24,50 @@ class Card
     end
   end
 
+  def num_of_cards_to_win
+    number_of_matches
+  end
+
+  def cards_to_win
+    number_of_matches.times.map{ |i| i + number + 1 }
+  end
+
   def to_s
-    "Card #{number}: #{winning.join(" ")} | #{chosen.join(" ")} | Score: #{score}"
+    "Card #{number}: #{winning.join(" ")} | #{chosen.join(" ")} | Matches: #{number_of_matches} | Score: #{score}"
+  end
+
+  private
+
+  def number_of_matches
+    chosen.select do |chosen_number|
+      winning.include?(chosen_number)
+    end.size
   end
 end
 
 cards = lines.map{ |line| Card.new(line) }
 puts cards
-puts "Total: #{cards.map(&:score).inject(&:+)}"
+puts "\nPart 1"
+puts "Total score: #{cards.map(&:score).inject(&:+)}"
+
+puts "\nPart 2"
+originals = cards.dup
+cards_and_copies = cards.dup
+
+# originals.each do |card|
+#   puts "Card #{card.number}: #{card.cards_to_win.join(", ")}"
+# end
+
+i = 0
+while i < cards_and_copies.length
+  card = cards_and_copies[i]
+  puts i.to_s if i % 100 == 0
+
+  card.cards_to_win.each do |index|
+    cards_and_copies.push(originals[index-1].dup)
+  end
+  
+  i += 1
+end
+
+puts cards_and_copies.size
